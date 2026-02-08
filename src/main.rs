cd553108dcbe006d7b985357378f8a2d9f832871
nicolaslferreira <nicolas.l.ferreira@gmail.com>
2026-02-09 01:12:31 +1300

    Renaming and removed Instant logging since I have proper benchmarking and profiling now

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use crossbeam::atomic::AtomicCell;
 use eframe::Renderer;
 use std::sync::Arc;
 use std::thread;
-use std::time::{Duration, Instant};
+use std::time::Duration;
 use tmfroc::conway;
 use tmfroc::thanatos;
 use tmfroc::types::cell_configuration::CellConfiguration;
@@ -21,44 +21,36 @@ fn main() {
     ];
 
     let seed_cells = CellConfiguration::random_configuration(42, 20, 20, 0.3);
-    let shared = Arc::new(AtomicCell::new(Arc::new(SimulationPayload::default())));
+    let feed = Arc::new(AtomicCell::new(Arc::new(SimulationPayload::default())));
 
     // Simulation thread
     {
-        let clone = Arc::clone(&shared);
-        thread::spawn(move || run_logic(glider, clone))
+        let clone = Arc::clone(&feed);
+        thread::spawn(move || start_simulation(glider, clone))
     };
 
-    run_ui(shared);
+    start_ui(Arc::clone(&feed));
 }
 
-fn run_logic(seed_cells: Vec<CellCoord>, shared: SimulationFeed) {
-    // Own the mutable working copy of the simulation state
+fn start_simulation(seed_cells: Vec<CellCoord>, feed: SimulationFeed) {
     let mut cconf = CellConfiguration::with_seed_configuration(seed_cells);
-
     loop {
-        let mut start = Instant::now();
-
         // Run Thanatos on current configuration
         thanatos::tmfroc::run(&cconf);
-        let elapsed = start.elapsed();
-        println!("Thanatos: {:?}", elapsed);
 
-        // Step Conway simulation
-        start = Instant::now();
+        // Step Conway
         let new_cconf = conway::step(&cconf);
-        cconf = new_cconf;
-        let elapsed = start.elapsed();
-        println!("Simulation: {:?}", elapsed);
 
-        // Publish a snapshot to UI (or other observers)
-        // Only the Arc is cloned, not the entire HashSet
-        shared.store(Arc::new(SimulationPayload::new(Some(cconf.clone()))));
-        thread::sleep(Duration::from_millis(500));
+        // Publish results
+        cconf = new_cconf;
+        feed.store(Arc::new(SimulationPayload::new(Some(cconf.clone()))));
+
+        // Currently capping for UI and development
+        thread::sleep(Duration::from_millis(2));
     }
 }
 
-fn run_ui(shared: SimulationFeed) {
+fn start_ui(feed: SimulationFeed) {
     let native_options = eframe::NativeOptions {
         centered: true,
         renderer: Renderer::Wgpu,
@@ -67,7 +59,7 @@ fn run_ui(shared: SimulationFeed) {
     eframe::run_native(
         "Thanatos CGoL",
         native_options,
-        Box::new(|cc| Ok(Box::new(App::new(cc, shared)))),
+        Box::new(|cc| Ok(Box::new(App::new(cc, feed)))),
     )
     .unwrap();
 }
