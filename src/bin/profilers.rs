2a42b51c1b173a0e44bb373e86f6d59db6d70c70
NicolasF <nicolasf@leapthought.com>
2026-02-09 18:37:37 +1300

    windows removed from profiling

--- src/bin/profilers.rs
+++ src/bin/profilers.rs
@@ -1,50 +1,33 @@
-use std::fs::File;
-use std::io::Write;
-use thanatos::types::cell_configuration::CellConfiguration;
-use thanatos::*;
-
+#[cfg(unix)]
 fn main() {
-    let seed = CellConfiguration::random_configuration(42, 10, 10, 0.2);
-    let cconf = CellConfiguration::with_seed_configuration(seed);
-
-    let guard = pprof2::ProfilerGuardBuilder::default()
+    let guard = pprof::ProfilerGuardBuilder::default()
         .frequency(10000)
+        .blocklist(&["libc", "libgcc", "pthread", "vdso"])
         .build()
         .unwrap();
 
+    let seed = thanatos::types::cell_configuration::CellConfiguration::random_configuration(
+        42, 10, 10, 0.2,
+    );
+    let cconf =
+        thanatos::types::cell_configuration::CellConfiguration::with_seed_configuration(seed);
     for _ in 0..10_000 {
-        conway::step(&cconf);
+        thanatos::conway::step(&cconf);
     }
 
     if let Ok(report) = guard.report().build() {
-        // Flamegraph SVG
-        let file = File::create("profile.svg").unwrap();
+        println!("Saving .svg");
+        let file = std::fs::File::create("tracing.svg").unwrap();
         report.flamegraph(file).unwrap();
-
-        // CSV summary
-        let mut csv = File::create("profile.csv").unwrap();
-        writeln!(csv, "function,samples").unwrap();
-
-        for (frames, count) in &report.data {
-            let func_path: String = frames
-                .frames
-                .iter()
-                .map(|stack| {
-                    stack
-                        .iter()
-                        .map(|s| {
-                            s.name
-                                .as_ref()
-                                .map(|v| String::from_utf8_lossy(v).to_string())
-                                .unwrap_or("<unknown>".to_string())
-                        })
-                        .collect::<Vec<_>>()
-                        .join(";")
-                })
-                .collect::<Vec<_>>()
-                .join("|"); // separate multiple stacks
-
-            writeln!(csv, "{},{}", func_path, count).unwrap();
-        }
     }
+}
+
+#[cfg(windows)]
+fn main() {
+    // screw them W*ndows users
+    eprintln!(
+        "Profiling support only for UNIX-based systems, '{}' is not supported.",
+        std::env::consts::OS
+    );
+    std::process::exit(0);
 }
